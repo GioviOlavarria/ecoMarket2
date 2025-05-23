70e2640dc637a767741f00a8f0c2f0092e080b91
Giovanni <gi.olavarria@duocuc.cl>
2025-05-22 21:27:52 -0400

    agregada dependencia Validation.

--- src/main/java/dev/ecomarket2/entidades/usuario.java
+++ src/main/java/dev/ecomarket2/entidades/usuario.java
@@ -2,20 +2,35 @@ package dev.ecomarket2.entidades;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
 
 @Data
 @Entity
 @Table(name = "tabla_usuarios")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_usuario;
 
+
+    @NotBlank(message = "El nombre de usuario no puede estar vacío")
+    @Column(nullable = false, length = 50)
     private String nombreUsuario;
 
+    @NotBlank(message = "El apellido de usuario no puede estar vacío")
+    @Column(nullable = false, length = 50)
     private String apellidoUsuario;
 
+    @NotBlank(message = "La contraseña no puede estar vacía")
     @Column(name = "correo_electronico", unique = true, nullable = false)
     private String email;
+
 }
