2198938ecfcdfcab85e227ef09f77d252e2643d0
Giovanni <gi.olavarria@duocuc.cl>
2025-05-22 20:38:59 -0400

    proyecto creado

---
+++ src/test/java/dev/ecomarket2/EcoMarket2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ecomarket2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMarket2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
